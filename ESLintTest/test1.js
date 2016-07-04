a697aba599cced387b2d44d8d5dae8445fe5dd8b
ragesh83 <ragesh83@gmail.com>
2016-07-04 14:08:34 +0530

    Added angular project

--- ESLintTest/test1.js
+++ ESLintTest/test1.js
@@ -15,3 +15,21 @@ var b;
 
 var d = e;
 
+
+unction Test() {
+  var a = 10;
+  ajhksf
+}
+
+function Test() {
+ console.log(2);
+  tets
+}
+if(true) alert("ok")
+var foo = bar;
+
+var test = test21;
+var b;
+
+var d = e;
+
